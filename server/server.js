da3f27bed908a397d839eaf0ec8e776dbe1f5d8d
Jacob Sipple <sipple3@gmail.com>
2019-03-09 13:24:58 -0500

    semi-finished functional not pretty

--- server/server.js
+++ server/server.js
@@ -6,49 +6,49 @@ const path= require("path");
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+const waitlist = []
+
+const reservations = [
+  {
+    id: "tomperson",
+    name: "Tom Person",
+    email: "tom@person.com",
+    phone: "919-555-0987"
+  },
+  {
+    id: "tomperson",
+    name: "Tom Person",
+    email: "tom@person.com",
+    phone: "919-555-0987"
+  },
+  {
+    id: "tomperson",
+    name: "Tom Person",
+    email: "tom@person.com",
+    phone: "919-555-0987"
+  }
+];
+
 app.get('/', function(req,res){
-    res.sendFile(path.join(__dirname, ""))
+    res.sendFile(path.join(__dirname, "../public/index.html"))
 });
 
-$("#submit").on("click", function(e){
- e.preventDefault();
- const newReservation = {
-     name:$("#name").val().trim(),
-     phoneNumber:$("#phoneNumber").val().trim(),
-     email:$("#email").val().trim(),
-     uniqueID:$("#uniqueID").val().trim()
- };
+app.get('/:Route', (req, res) => {
+    let route = req.params.Route
+    res.sendFile(path.join(__dirname, "../public/" + route + ".html"))
+})
 
- if(reservations.length<6){
-     $.post("/api/reservations", newReservation)
-     .then(function(data){
-         console.log(data);
-         alert("Your reservation has been made");
-         document.location.href= "/";
-     });
- }else{
-     $.post("/api/waitlist", newReservation)
-     .then(function(data){
-         console.log(data);
-         alert("Sorry, we are full. You are on the waitlist");
-         document.location.href= "/";
-     });
- };
+app.get("/api/reservation", function(req, res) {
+    console.log(reservations);
+    return res.json(reservations);
 });
 
-
-$("#viewTables").on("click", function(e){
- e.preventDefault();
- $.get("/api/reservations", function(data){
-     console.log(data);
- });
- $.get("/api/waitlist", function(data){
-     console.log(data);
- });
-});
-
-
-
+app.post('/api/reservation', (req,res) => {
+    let newReservation = req.body
+    console.log(newReservation);
+    reservations.push(newReservation)
+    res.json(newReservation)
+})
 
 app.listen(PORT, function(){
  console.log("you are listening on: "+ PORT);
