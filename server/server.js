f8599a2136cf176e6f00c9e5e1fd736228200728
Jacob Sipple <sipple3@gmail.com>
2019-03-09 11:36:26 -0500

    added some javascript and submit button

--- server/server.js
+++ server/server.js
@@ -12,9 +12,38 @@ app.get('/', function(req,res){
 
 app.get('/:route', (req, res) => {
  let path = req.params.route
- 
+
 })
 
 app.listen(PORT, function(){
     console.log("you are listening on: "+ PORT);
 })
+
+$("#submit").on("click", function(e){
+ e.preventDefault();
+ const newReservation= {
+     name:$("#name").val().trim(),
+     phoneNumber:$("#phoneNumber").val().trim(),
+     email:$("#email").val().trim(),
+     uniqueID:$("#uniqueID").val().trim()
+ };
+
+ if(reservations.length<6){
+     $.post("/api/reservations", newReservation)
+     .then(function(data){
+         console.log(data);
+         alert("Your reservation has been made");
+         document.location.href= "/";
+     });
+ }else{
+     $.post("/api/waitlist", newReservation)
+     .then(function(data){
+         console.log(data);
+         alert("Sorry, we are full. You are on the waitlist");
+         document.location.href= "/";
+     });
+ };
+});
+
+
+
